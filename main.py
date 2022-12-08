49fa2b3296cac8929fee0ac98e66988d3ed7425c
trenten2005@gmail.com <trenten2005@gmail.com>
2022-12-08 11:28:57 -0600

    added button test

--- main.py
+++ main.py
@@ -27,7 +27,36 @@ async def pog(ctx: interactions.CommandContext):
 async def relay(ctx: interactions.CommandContext, text: str):
     """relay what u say"""
     await ctx.send(f"You said '{text}'!")
+    
+    
+ 
+button = interactions.Button(
+    style=interactions.ButtonStyle.PRIMARY,
+    label="hello bot!",
+    custom_id="hello"
+)
+button2 = interactions.Button(
+    style=interactions.ButtonStyle.DANGER,
+    label="bye bot!",
+    custom_id="bye",
+)
 
+row = interactions.ActionRow.new(button1, button2)
+
+@bot.command()
+async def button_test(ctx):
+  """a test for buttons"""
+  await ctx.send("testing", components=row)
+  
+@bot.component("hello")
+async def button_response(ctx):
+    await ctx.send("Hello user :)", ephemeral=True)
+    
+@bot.component("bye")
+async def button2_response(ctx):
+  await ctx.send("aww bye bye user :(", ephemeral=True)
+  
+  
     
 
 
